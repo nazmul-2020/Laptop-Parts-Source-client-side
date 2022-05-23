1c84be10bc96ae08fa7d68d50e3af693d7b54537
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-05-23 20:09:01 +0600

    home page summary

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import Banner from '../../Banner';
+import Footer from '../Shared/Navbar/Footer';
+import Banner from './Banner';
+import Contact from './Contact';
+import PhotoGallery from './PhotoGallery';
+import Products from './Products';
+import Reviews from './Reviews';
 
 const Home = () => {
     return (
         <div>
                 <h1>Home</h1>
                 <Banner></Banner>
+                <Products></Products>
+                <Reviews></Reviews>
+                <Contact></Contact>
+                <PhotoGallery></PhotoGallery>
+                <Footer></Footer>
         </div>
     );
 };
---
+++ src/Pages/Home/PhotoGallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PhotoGallery = () => {
+    return (
+        <div>
+            <h2>PhotoGallery</h2>
+        </div>
+    );
+};
+
+export default PhotoGallery;
